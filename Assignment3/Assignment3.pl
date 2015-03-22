987f70a487caaf5879e7aab441b51f7509a0a5f9
Andrew Fontaine <andrew@afontaine.ca>
2015-03-22 17:34:25 -0600

    A3Q3B working.

--- Assignment3/Assignment3.pl
+++ Assignment3/Assignment3.pl
@@ -13,3 +13,35 @@ swap([], []).
 swap([A], [A]).
 swap([A, B|L], [B, A|R]) :-
 	swap(L, R).
+
+% rmDup takes in a list and returns the list with all duplicates removed.
+rmDup([], []).
+rmDup([A|L], R) :-
+	member(A, L),
+	rmDup(L, R).
+rmDup([A|L], [A|R]) :-
+	rmDup(L, R).
+
+% rmAllDup removes duplicates from a list with nested lists.
+rmAllDup([], [], S).
+rmAllDup([A|L], [X|R], S) :-
+	\+ atomic(A),
+	append([], S, T),
+	rmAllDup(A, X, T),
+	flatten(X, Y),
+	append(S, Y, Z),
+	rmAllDup(L, R, Z).
+rmAllDup([A|L], [A|R], S) :-
+	\+ member(A, S),
+	append(S, [A], T),
+	rmAllDup(L, R, T).
+rmAllDup([_|L], R, S) :-
+	rmAllDup(L, R, S).
+rmAllDup(L, R) :-
+	rmAllDup(L, R, []).
+
+flatten([], []).
+flatten([A|L], [A|R]) :-
+	atomic(A), flatten(L, R).
+flatten([A|L], R) :-
+	flatten(A, A1), flatten(L, L1), append(A1, L1, R).
