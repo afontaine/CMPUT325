d2bf75e01827bd09c8ad5c760524e4027b0f84c3
Andrew Fontaine <andrew@afontaine.ca>
2015-04-10 11:59:19 -0600

    A4Q1C done. Now for constraints.

--- Assignment4/Assignment4.pl
+++ Assignment4/Assignment4.pl
@@ -1,24 +1,25 @@
 % Data for testing purposes.
-c325(fall_2014,aperf,15,15,15,15,79,99).
-c325(fall_2014,john,14,13,15,10,76,87).
-c325(fall_2014,lily, 9,12,14,14,76,92).
-c325(fall_2014,peter,8,13,12,9,56,58).
-c325(fall_2014,ann,14,15,15,14,76,95).
-c325(fall_2014,ken,11,12,13,14,54,87).
-c325(fall_2014,kris,13,10,9,7,60,80).
-c325(fall_2014,audrey,10,13,15,11,70,80).
-c325(fall_2014,randy,14,13,11,9,67,76).
-c325(fall_2014,david,15,15,11,12,66,76).
-c325(fall_2014,sam,10,13,10,15,65,67).
-c325(fall_2014,kim,14,13,12,11,68,78).
-c325(fall_2014,perf,15,15,15,15,80,100).
-c325(winter_2014,aperf,15,15,15,15,80,99).
-setup(fall_2014,as1,15,0.1).
-setup(fall_2014,as2,15,0.1).
-setup(fall_2014,as3,15,0.1).
-setup(fall_2014,as4,15,0.1).
-setup(fall_2014,midterm,80,0.25).
-setup(fall_2014,final,100,0.35).
+insert_data :-
+	assert(c325(fall_2014,aperf,15,15,15,15,79,99)),
+    assert(c325(fall_2014,john,14,13,15,10,76,87)),
+    assert(c325(fall_2014,lily, 9,12,14,14,76,92)),
+    assert(c325(fall_2014,peter,8,13,12,9,56,58)),
+    assert(c325(fall_2014,ann,14,15,15,14,76,95)),
+    assert(c325(fall_2014,ken,11,12,13,14,54,87)),
+    assert(c325(fall_2014,kris,13,10,9,7,60,80)),
+    assert(c325(fall_2014,audrey,10,13,15,11,70,80)),
+    assert(c325(fall_2014,randy,14,13,11,9,67,76)),
+    assert(c325(fall_2014,david,15,15,11,12,66,76)),
+    assert(c325(fall_2014,sam,10,13,10,15,65,67)),
+    assert(c325(fall_2014,kim,14,13,12,11,68,78)),
+    assert(c325(fall_2014,perf,15,15,15,15,80,100)),
+    assert(c325(winter_2014,aperf,15,15,15,15,80,99)),
+    assert(setup(fall_2014,as1,15,0.1)),
+    assert(setup(fall_2014,as2,15,0.1)),
+    assert(setup(fall_2014,as3,15,0.1)),
+    assert(setup(fall_2014,as4,15,0.1)),
+    assert(setup(fall_2014,midterm,80,0.25)),
+    assert(setup(fall_2014,final,100,0.35)).
 
 query1(S, N, T) :-
 	setup(S, as1, M1, W1),
@@ -38,4 +39,31 @@ query2name(S, N) :-
 	setup(S, final, MF, _),
 	c325(S, N, _, _, _, _, M, F),
 	M / MM < F / MF.
+
+query3(S, N, as1, X) :-
+	c325(S, N, A1, A2, A3, A4, M, F),
+	retract(c325(S, N, A1, A2, A3, A4, M, F)),
+	assert(c325(S, N, X, A2, A3,A4, M, F)).
+query3(S, N, as2, X) :-
+	c325(S, N, A1, A2, A3, A4, M, F),
+	retract(c325(S, N, A1, A2, A3, A4, M, F)),
+	assert(c325(S, N, A1, X, A3, A4, M, F)).
+query3(S, N, as3, X) :-
+	c325(S, N, A1, A2, A3, A4, M, F),
+	retract(c325(S, N, A1, A2, A3, A4, M, F)),
+	assert(c325(S, N, A1, A2, X, A4, M, F)).
+query3(S, N, as4, X) :-
+	c325(S, N, A1, A2, A3, A4, M, F),
+	retract(c325(S, N, A1, A2, A3, A4, M, F)),
+	assert(c325(S, N, A1, A2, A3, X, M, F)).
+query3(S, N, midterm, X) :-
+	c325(S, N, A1, A2, A3, A4, M, F),
+	retract(c325(S, N, A1, A2, A3, A4, M, F)),
+	assert(c325(S, N, A1, A2, A3, A4, X, F)).
+query3(S, N, final, X) :-
+	c325(S, N, A1, A2, A3, A4, M, F),
+	retract(c325(S, N, A1, A2, A3, A4, M, F)),
+	assert(c325(S, N, A1, A2, A3, A4, M, X)).
+query3(_, _, _, _) :-
+	print('record not found').
 
